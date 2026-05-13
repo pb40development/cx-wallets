274f6fd732c82eb84d6f8fe18b057a4e9704a583
Zead-Alshu22 <zead.alshukairi@planen-bauen40.de>
2026-05-13 08:25:18 +0200

    removing deployment and api files from this repo

--- launcher/identityhub-cx/additional_config/pg_init/pg_init.sql
+++
@@ -1,8 +0,0 @@
-    CREATE DATABASE iss_db;
-    CREATE DATABASE cons_ih_db;
-    CREATE DATABASE prov_ih_db;
-    CREATE DATABASE cons_cpl;
-    CREATE DATABASE cons_dpl;
-    CREATE DATABASE prov_cpl;
-    CREATE DATABASE prov_dpl;
-
